31023556abf4da4e3d06d14f9febc1c23aa3f8d0
Khalil Stroman <7208392@philasd.org>
2020-09-18 22:59:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Author: Khalil Stroman kzs5955@psu.edu
+def digit_sum(n):
+  if n > 0:
+    return n%10 + int(digit_sum(n/10))
+  else: 
+    return 0
+
+def run():
+  digits = input("Enter an int: ")
+  digits = int(digits)
+  print(f"sum of digits {digits} is {digit_sum(digits)}.")
+
+if __name__ == "__main__":
+  run()
